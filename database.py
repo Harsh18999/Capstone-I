0dd32e45c50eb2e176f3a4d81cc8aa61fe23fa67
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-06-23 19:54:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,4 @@
 import psycopg2
 import os
-conn = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=require&sslrootcert=root.crt"
-)
-#$env:DATABASE_URL = "cockroachdb://myproject20.com:<ZNfo9DxeFp-WoNzpTDJPmg>@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full"
-conn_str="postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=require&sslrootcert=root.crt"
+conn = psycopg2.connect(os.environ["conn_str"])
+conn_str=os.environ["conn_str"]
