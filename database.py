5f1647b54570bf8dab2d2f0f21431b228cf7945c
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-06-23 19:38:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 import psycopg2
 
-conn = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=require&sslrootcert=root.crt")
+conn = psycopg2.connect(
+    dbname="project",
+    user="MYPROJECT20.COM",
+    password="ZNfo9DxeFp-WoNzpTDJPmg",
+    host="almond-heron-1166.j77.cockroachlabs.cloud",
+    port="26257",
+    sslmode="verify-full",
+    sslrootcert="/root.crt"
+)
 #$env:DATABASE_URL = "cockroachdb://myproject20.com:<ZNfo9DxeFp-WoNzpTDJPmg>@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full"
 conn_str="postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=require&sslrootcert=root.crt"
