8fc81767acf92f60f6198fb507bee4ad171f420b
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-07-01 15:25:37 +0530

    Delete database.py

--- database.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full")
-#$env:conn_str = cockroachdb://myproject20.com:<ZNfo9DxeFp-WoNzpTDJPmg>@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full
-conn_str="postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full"
